2b18f30107fb93204fedc409a9a938cca144ef0c
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-01-20 23:14:15 +0900

    [FEAT] : Make OnDraw, OnSizeChanged

--- app/src/main/java/com/hyunwoo/customviewpractice/DialView.kt
+++ app/src/main/java/com/hyunwoo/customviewpractice/DialView.kt
@@ -1,11 +1,12 @@
 package com.hyunwoo.customviewpractice
 
 import android.content.Context
-import android.graphics.Paint
-import android.graphics.PointF
-import android.graphics.Typeface
+import android.graphics.*
 import android.util.AttributeSet
 import android.view.View
+import kotlin.math.cos
+import kotlin.math.min
+import kotlin.math.sin
 
 private enum class FanSpeed(val label: Int) {
     OFF(R.string.fan_off),
@@ -31,5 +32,34 @@ class DialView @JvmOverloads constructor(
         textAlign = Paint.Align.CENTER
         textSize = 55.0f
         typeface = Typeface.create("", Typeface.BOLD)
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        radius = (min(w, h) / 2.0 * 0.8).toFloat()
+    }
+
+    private fun PointF.computeXYForSpeed(pos: FanSpeed, radius: Float) {
+        val startAngle = Math.PI * (9 / 8.0)
+        val angle = startAngle + pos.ordinal * (Math.PI / 4)
+        x = (radius * cos(angle)).toFloat() + width / 2
+        y = (radius * sin(angle)).toFloat() + height / 2
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        paint.color = if (fanSpeed == FanSpeed.OFF) Color.GRAY else Color.GREEN
+        canvas.drawCircle((width / 2).toFloat(), (height / 2).toFloat(), radius, paint)
+
+        val markerRadius = radius + RADIUS_OFFSET_INDICATOR
+        pointPosition.computeXYForSpeed(fanSpeed, markerRadius)
+        paint.color = Color.BLACK
+        canvas.drawCircle(pointPosition.x, pointPosition.y, radius / 12, paint)
+
+        val labelRadius = radius + RADIUS_OFFSET_LABEL
+        FanSpeed.values().forEach { it ->
+            pointPosition.computeXYForSpeed(it, labelRadius)
+            val label = resources.getString(it.label)
+            canvas.drawText(label, pointPosition.x, pointPosition.y, paint)
+        }
     }
 }
